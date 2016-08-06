91baa5d90badec37af56ef7bde02322a9276272b
Brian Voskerijian <bvoskerijian@gmail.com>
2016-08-06 14:47:56 -0400

    Add delete action in Note controller

--- web/controllers/notes_controller.ex
+++ web/controllers/notes_controller.ex
@@ -49,4 +49,13 @@ defmodule Notox.NoteController do
 
     render conn, :show, note: note
   end
+
+  def delete(conn, %{"id" => id}) do
+    Repo.get!(Note, id)
+    |> Repo.delete
+
+    conn
+    |> put_flash(:info, "Note deleted.")
+    |> redirect(to: note_path(conn, :index))
+  end
 end
