c28f3862b02319a2a8f7e4fb2e59c0c2cafc0d17
Brian Voskerijian <bvoskerijian@gmail.com>
2016-08-19 16:02:21 -0400

    Setup deployment to Heroku
    - Use secret key from Heroku enviroment variables in prod.exs
    - Add production database configuration to prod.exs
    - Add Procfile to start application

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,20 @@ use Mix.Config
 # which you typically run after static files are built.
 config :notox, Notox.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [
+    scheme: "https",
+    host: "immense-cliffs-79270.herokuapp.com",
+    port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure database
+config :notox, Notox.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,7 +71,3 @@ config :logger, level: :info
 # for the new static assets to be served after a hot upgrade:
 #
 #     config :notox, Notox.Endpoint, root: "."
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
